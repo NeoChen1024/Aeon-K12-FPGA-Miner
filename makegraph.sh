55bdaa63486317a221a9a84f3261e453b0e19691
Neo_Chen <chenkolei@gmail.com>
2022-03-31 20:46:53 +0800

    arch.png: up

--- makegraph.sh
+++ makegraph.sh
@@ -4,6 +4,9 @@ set -xe
 
 if which dot 2>&1 > /dev/null ; then
 	dot -T png arch.dot -o arch.png
+	if which optipng 2>&1 > /dev/null; then
+		optipng -o7 -v arch.png
+	fi
 else
 	echo 'You need to install graphviz first'
 fi
